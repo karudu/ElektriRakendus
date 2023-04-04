ef992451026436dc7b0d7375462d95a2e4476fa8
Karl Udu <karudu@ttu.ee>
2023-04-04 13:51:41 +0300

    Parandatud projekt

--- ElektriRakendus/Form1.vb
+++ ElektriRakendus/Form1.vb
@@ -1,14 +1,3 @@
 ﻿Public Class Form1
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form3.Show()
-    End Sub
->>>>>>> parent of e93bfd1 (ei saa aru kuidas saada strucktist comboboox itemid)
-=======
->>>>>>> parent of 4ac4c7a (Kalukulaatoor(Hinna kalkulaator ja  Kodumasina kalkulaator form) algne mudel)
 End Class
